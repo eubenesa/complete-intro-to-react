5163f96585d890457c1b1525d894cbe5b27ef8a5
Eubene Sa <eubene.sa@gmail.com>
2017-05-30 11:57:10 -0500

    Use default webpack bundle

--- src/ClientApp.jsx
+++ src/ClientApp.jsx
@@ -1,20 +1,17 @@
+import React from 'react';
+import { render } from 'react-dom';
+
 const ce = React.createElement;
 
-const MyTitle = props => {
-  return ce(
-    "div",
-    null,
-    ce("h1", { style: { color: props.color } }, props.title)
-  );
-};
+const MyTitle = props =>
+  ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
 
-const MyFirstComponent = () => {
-  return ce("div", { id: "my-first-component" }, [
-    ce(MyTitle, { title: "Game of Thrones", color: "YellowGreen" }),
-    ce(MyTitle, { title: "Stranger Things", color: "GreenYellow" }),
-    ce(MyTitle, { title: "Silicon Valley", color: "LimeGreen" }),
-    ce(MyTitle, { title: "Unbreakable Kimmy Schmidt", color: "peru" })
+const MyFirstComponent = () =>
+  ce('div', { id: 'my-first-component' }, [
+    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
+    ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
+    ce(MyTitle, { title: 'Silicon Valley', color: 'LimeGreen' }),
+    ce(MyTitle, { title: 'Unbreakable Kimmy Schmidt', color: 'peru' })
   ]);
-};
 
-ReactDOM.render(ce(MyFirstComponent), document.getElementById("app"));
+render(ce(MyFirstComponent), document.getElementById('app'));
