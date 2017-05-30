23643eb766ff401195621180be1c24ce851b0c66
Eubene Sa <eubene.sa@gmail.com>
2017-05-30 13:43:28 -0500

    Start getting into JSX

--- src/ClientApp.jsx
+++ src/ClientApp.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const ce = React.createElement;
+const MyTitle = props => (
+  <div>
+    <h1 style={{ color: props.color }}>{props.title.toUpperCase()}</h1>
+  </div>
+  // <h2>will work in React 16</h2>
+);
 
-const MyTitle = props =>
-  ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
+const MyFirstComponent = () => (
+  <div id="my-first-component">
+    <MyTitle title="Game of Thrones" color="YellowGreen" />
+    <MyTitle title="Stranger Things" color="GreenYellow" />
+    <MyTitle title="House of Cards" color="Peru" />
+    <MyTitle title="Unbreakable Kimmy Schmidt" color="burlywood" />
+  </div>
+);
 
-const MyFirstComponent = () =>
-  ce('div', { id: 'my-first-component' }, [
-    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'House of Cards', color: 'LimeGreen' }),
-    ce(MyTitle, { title: 'Unbreakable Kimmy Schmidt', color: 'peru' })
-  ]);
-
-render(ce(MyFirstComponent), document.getElementById('app'));
+render(<MyFirstComponent />, document.getElementById('app'));
