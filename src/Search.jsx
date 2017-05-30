0921cd79ac7e040d64236d6e2f38bd34ce981548
Eubene Sa <eubene.sa@gmail.com>
2017-05-30 16:52:10 -0500

    Use class properties, no more .bind(this)!

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import preload from '../data.json';
 
 import ShowCard from './ShowCard';
 
-const Search = () => (
-  <div className="search">
-    <div>
-      {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
-    </div>
-  </div>
-);
+class Search extends Component {
+  state = {
+    searchTerm: ''
+  };
+
+  handleSearchTermChange = event => {
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input
+            type="text"
+            value={this.state.searchTerm}
+            placeholder="Search ..."
+            onChange={this.handleSearchTermChange}
+          />
+        </header>
+        <div>
+          {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Search;
