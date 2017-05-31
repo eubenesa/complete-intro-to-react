4384eba037756116514c026286097534c9702d5c
Eubene Sa <eubene.sa@gmail.com>
2017-05-31 11:12:16 -0500

    Add support for Hot Module Replacement

--- src/ClientApp.jsx
+++ src/ClientApp.jsx
@@ -1,22 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import App from './App';
 
-import Landing from './Landing';
-import Search from './Search';
+const renderApp = () => {
+  render(<App />, document.getElementById('app'));
+};
+renderApp();
 
-const FourOFour = () => <h1>404</h1>;
-
-const App = () => (
-  <BrowserRouter>
-    <div className="app">
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route path="/search" component={Search} />
-        <Route component={FourOFour} />
-      </Switch>
-    </div>
-  </BrowserRouter>
-);
-
-render(<App />, document.getElementById('app'));
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    renderApp();
+  });
+}
