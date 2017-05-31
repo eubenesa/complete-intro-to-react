d21b7a1a04c2ff1fab5e63f38f3fa64106d6e3ed
Eubene Sa <eubene.sa@gmail.com>
2017-05-31 15:00:01 -0500

    Add linking from Search to Details

--- src/ShowCard.jsx
+++ src/ShowCard.jsx
@@ -1,15 +1,19 @@
 // @flow
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-const Wrapper = styled.div`
+// $FlowFixMe
+const Wrapper = styled(Link)`
   width: 32%;
   border: 2px solid #333;
   border-radius: 4px;
   margin-bottom: 25px;
   padding-right: 10px;
   overflow: hidden;
+  color: black;
+  text-decoration: none;
 `;
 
 const Image = styled.img`
@@ -18,13 +22,8 @@ const Image = styled.img`
   margin-right: 10px;
 `;
 
-const ShowCard = (props: {
-  poster: string,
-  title: string,
-  year: string,
-  description: string
-}) => (
-  <Wrapper>
+const ShowCard = (props: Show) => (
+  <Wrapper to={`/details/${props.imdbID}`}>
     <Image
       alt={`${props.title} Show Poster`}
       src={`/public/img/posters/${props.poster}`}
