c978a189e7d3943419d651b570295d26815021d2
Eubene Sa <eubene.sa@gmail.com>
2017-06-01 10:09:56 -0500

    Get Flow working with Redux

--- src/actionCreaters.js
+++ src/actionCreaters.js
@@ -1,6 +1,8 @@
+// @flow
+
 import { SET_SEARCH_TERM } from './actions';
 
-export const setSearchTerm = searchTerm => ({
+export const setSearchTerm = (searchTerm: string) => ({
   type: SET_SEARCH_TERM,
   payload: searchTerm
 });
